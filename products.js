6efe409d7a49c40f3c9ba2176ee65aca55b00d49
vaneezaniazi5-cpu <vaneezaniazi5@gmail.com>
2026-07-10 15:03:24 +0500

    Update products.js

--- products.js
+++ products.js
@@ -1,1 +1,43 @@
+const products = [
 
+{
+    name: "Maria. B. Lawn MPT 3010-A",
+    fabric: "Double Shaded Printed Lawn Shirt<br>Printed Chiffon Dupatta<br>Printed Trouser",
+    price: 3950,
+    oldPrice: 5750,
+    badge: "SALE",
+    front: "images/IMG-20260706-WA0019.jpg",
+    back: "images/IMG-20260706-WA0066.jpg"
+},
+
+{
+    name: "Maria. B. Lawn MPT 3010-B",
+    fabric: "Double Shaded Printed Lawn Shirt<br>Printed Chiffon Dupatta<br>Printed Trouser",
+    price: 3950,
+    oldPrice: 5750,
+    badge: "SALE",
+    front: "images/IMG-20260706-WA0058.jpg",
+    back: "images/IMG-20260706-WA0043.jpg"
+},
+
+{
+    name: "Amaltas 1608",
+    fabric: "",
+    price: 4995,
+    oldPrice: null,
+    badge: "NEW",
+    front: "images/IMG-20260706-WA0013.jpg",
+    back: "images/IMG-20260706-WA0041.jpg"
+},
+
+{
+    name: "Ramsha Lawn RS02",
+    fabric: "",
+    price: 3850,
+    oldPrice: null,
+    badge: "",
+    front: "images/Ramsha-lawn-RS02.jpg",
+    back: "images/Ramsha-lawn-RS02(2).jpg"
+}
+
+];
